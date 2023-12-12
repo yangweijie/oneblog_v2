d0077e223f32a6182ecda4f184fa1ba9cbad7276
yangweijie <917647288@qq.com>
2023-12-13 02:34:38 +0800

    多应用本地话，初始化海豚后台

--- config/app.php
+++ config/app.php
@@ -3,24 +3,29 @@
 // | 应用设置
 // +----------------------------------------------------------------------
 
+use think\facade\Env;
+
 return [
     // 应用地址
-    'app_host'         => env('APP_HOST', ''),
+    'app_host'         => Env::get('app.host', ''),
     // 应用的命名空间
     'app_namespace'    => '',
     // 是否启用路由
     'with_route'       => true,
-    // 默认应用
-    'default_app'      => 'index',
-    // 默认时区
-    'default_timezone' => 'Asia/Shanghai',
-
+    // 是否启用事件
+    'with_event'       => true,
+    // 自动多应用模式
+    'auto_multi_app'   => true,
     // 应用映射（自动多应用模式有效）
     'app_map'          => [],
     // 域名绑定（自动多应用模式有效）
     'domain_bind'      => [],
     // 禁止URL访问的应用列表（自动多应用模式有效）
     'deny_app_list'    => [],
+    // 默认应用
+    'default_app'      => 'index',
+    // 默认时区
+    'default_timezone' => 'Asia/Shanghai',
 
     // 异常页面的模板文件
     'exception_tmpl'   => app()->getThinkPath() . 'tpl/think_exception.tpl',
@@ -28,5 +33,24 @@ return [
     // 错误显示信息,非调试模式有效
     'error_message'    => '页面错误！请稍后再试～',
     // 显示错误信息
-    'show_error_msg'   => false,
+    'show_error_msg'   => true,
+
+    // 后台公共模板
+    'admin_base_layout'  => app_path() . 'admin/view/layout.html',
+
+    // 插件目录路径
+    'plugin_path'        => root_path().'extend/plugins/',
+
+    // 数据包目录路径
+    'packet_path'        => root_path(). 'packet/',
+    // 文件上传路径
+    'upload_path'        => root_path() . 'public' . DIRECTORY_SEPARATOR . 'uploads',
+    // 文件上传临时目录
+    'upload_temp_path'   => root_path() . 'public' . DIRECTORY_SEPARATOR . 'uploads' . DIRECTORY_SEPARATOR . 'temp/',
+
+    // 最大缓存用户数
+    'user_max_cache' => 1000,
+    // 管理员用户ID
+    'user_admin'     => 1,
+
 ];
---
+++ public/admin.php
@@ -0,0 +1,31 @@
+<?php
+// +----------------------------------------------------------------------
+// | ThinkPHP [ WE CAN DO IT JUST THINK ]
+// +----------------------------------------------------------------------
+// | Copyright (c) 2006-2019 http://thinkphp.cn All rights reserved.
+// +----------------------------------------------------------------------
+// | Licensed ( http://www.apache.org/licenses/LICENSE-2.0 )
+// +----------------------------------------------------------------------
+// | Author: liu21st <liu21st@gmail.com>
+// +----------------------------------------------------------------------
+
+// [ 应用入口文件 ]
+namespace think;
+
+require __DIR__ . '/../vendor/autoload.php';
+define('IS_VERCEL', $_ENV['VERCEL']??0);
+define('APP_PATH', __DIR__ . '/../app/');
+// 定义入口为admin
+define('ENTRANCE', 'admin');
+// 执行HTTP应用并响应
+$app = (new App());
+if(IS_VERCEL){
+    $app->setRuntimePath(sys_get_temp_dir().DIRECTORY_SEPARATOR);
+}
+$http = $app->setEnvName('admin')->http;
+
+$response = $http->run();
+
+$response->send();
+
+$http->end($response);

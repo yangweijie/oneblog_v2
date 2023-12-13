ba78e955d38cb13ef90d46cecc23f813fb9722df
yangweijie <917647288@qq.com>
2023-12-13 20:37:27 +0800

    调试

--- api/index.php
+++ api/index.php
@@ -1,11 +1,2 @@
 <?php
-/**
- * Here is the serverless function entry
- * for deployment with Vercel.
- */
-
-if(stripos($_SERVER['REQUEST_URI'], 'admin.php') !== false){
-    require __DIR__.'/../public/admin.php';
-}else{
-    require __DIR__.'/../public/index.php';
-}
+require __DIR__.'/../public/index.php';
